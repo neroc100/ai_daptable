a0649848810075cc4048dd63c5c1e7bd2cabfb06
saurluca <spuckteller@gmail.com>
2025-08-12 15:40:37 +0200

    init: with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='bg-green-500 w-full h-screen'>  
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md'>Click me</button>
+    </div>
+  )
+}
+
+export default App
