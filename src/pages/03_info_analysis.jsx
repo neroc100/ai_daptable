9655d1078ae4803781b46438f61913839959b763
deltamoe <jeelka.hessenius@gmail.com>
2025-08-28 11:52:07 +0200

    Remove obsolete AI Action Selection and Progress components; update imports in Info_analysis page to reflect changes.

--- src/pages/03_info_analysis.jsx
+++ src/pages/03_info_analysis.jsx
@@ -6,7 +6,7 @@ import Progress_Bar from '../components/00 General_Page_Content/Progress_Bar';
 import Acquired_Info_Display from '../components/03 AI_Info_Acquisition/Acquired_Info_Display';
 import AI_info_Acq_box from '../components/03 AI_Info_Acquisition/AI_info_Acq_box';
 import AI_info_ana_box from '../components/04 AI_Info_Analysis/AI_info_ana_box';
-import Highlight_Malicious_Display from '../components/05 AI_Action_Selection/Highlight_Malicious_Display';
+import Analyzed_Info_Display from '../components/04 AI_Info_Analysis/Analyzed_Info_Display';
 import Allow_Button from '../components/02 Human_Action_Implementation/Allow_Button';
 import Block_Button from '../components/02 Human_Action_Implementation/Block_Button';
 
@@ -84,9 +84,9 @@ function Info_analysis() {
           )}
         </div>
         
-        {/* Highlight Malicious Display - Shows when AI analysis timer runs out */}
+        {/* Analyzed Info Display - Shows when AI analysis timer runs out */}
         {!isAnalysisLoading && (
-          <Highlight_Malicious_Display />
+          <Analyzed_Info_Display />
         )}
         
         {/* Acquired Information Display - Shows when acquisition is complete but hides when analysis is complete */}
