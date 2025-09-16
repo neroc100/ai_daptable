7c382e55f3fcb610cbee475ae8776a9d500f711a
deltamoe <jeelka.hessenius@gmail.com>
2025-09-16 10:42:55 +0200

    remove: access parts from @database.py

--- backend/database.py
+++ backend/database.py
@@ -20,24 +20,6 @@ class Trial(SQLModel, table=True):
     created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 
-# Example trials with different URLs and ratings
-example_trial = Trial(participant_id="123", condition=1, mental_effort_rating=75, url="https://www.google.com")
-example_trial_2 = Trial(participant_id="123", condition=2, mental_effort_rating=85, url="https://www.github.com")
-example_trial_3 = Trial(participant_id="123", condition=3, mental_effort_rating=95, url="https://www.stackoverflow.com")
-example_trial_4 = Trial(participant_id="123", condition=4, mental_effort_rating=65, url="https://www.reddit.com")
-example_trial_5 = Trial(participant_id="123", condition=5, mental_effort_rating=110, url="https://www.youtube.com")
-example_trial_6 = Trial(participant_id="123", condition=6, mental_effort_rating=90, url="https://www.wikipedia.org")
-
-SQLModel.metadata.create_all(engine)
-
-with Session(engine) as session:
-    session.add(example_trial)
-    session.add(example_trial_2)
-    session.add(example_trial_3)
-    session.add(example_trial_4)
-    session.add(example_trial_5)
-    session.add(example_trial_6)
-    session.commit()
 
 
 # %%
